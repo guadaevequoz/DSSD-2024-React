a8771d79488f8766d814d3f07a4a561422568063
Guadalupe Evequoz <guadalupe.evequoz@gmail.com>
2024-09-14 16:56:21 -0300

    Se inicia con la página

--- ecocycle/src/pages/Form.js
+++ ecocycle/src/pages/Form.js
@@ -1,5 +1,14 @@
 const Form = () => {
-  return <div>form</div>;
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold mb-4">Nuevo recorrido</h1>
+      <hr />
+      <div className="row">
+        <span className="text-lg">Materiales</span>
+        <button className="align-self-end bg-teal">+</button>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
--- ecocycle/src/pages/Home.js
+++ ecocycle/src/pages/Home.js
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div>Holis</div>;
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold mb-4">Bienvenido a Ecocycle🍃</h1>
+      <p className="text-lg">Sé parte del proceso de economía circular 🐛🚬</p>
+    </div>
+  );
 };
 
 export default Home;
